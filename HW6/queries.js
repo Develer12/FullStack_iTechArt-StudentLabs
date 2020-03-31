9f578e33e391833798433cef045009a7951dc83e
Develer12 <cenia-v@mail.ru>
2020-03-31 20:42:01 +0300

    Need to make some fixes

---
+++ HW6/queries.js
@@ -0,0 +1,70 @@
+const sql = require('mssql');
+
+let connectionPool;
+const config ={
+  user:'Develer12',
+  password: 'admin',
+  server:'DESKTOP-U4BLHC6', Database:'Nodejs'
+};
+let dbname = 'use Nodejs;';
+
+class DB {
+    constructor(){
+        connectionPool = new sql.ConnectionPool(config).connect()
+        .then(pool =>{
+          console.log('Connected to MSSQL');
+          return pool;
+        })
+        .catch(err => console.log('Connection Failed: ', err));
+    }
+
+    Get(tab){
+        return connectionPool.then(pool => {
+            return pool.query(`${dbname} SELECT * FROM ${tab}`);
+        });
+    }
+
+    Update(tab, fields){
+        return connectionPool.then(pool =>{
+            let req = pool.request();
+            let command = `${dbname} UPDATE ${tab} SET `;
+            let tabid = '';
+            Object.keys(fields).forEach(field =>{
+                if (field != tab)
+                    command += `${field} = '${fields[field]}',`;
+                else tabid = fields[field];
+            });
+            command = command.slice(0, -1);
+            command += ` WHERE ${tab} = '${tabid}'`;
+            console.log('|'+command+'|');
+
+            return req.query(command);
+        });
+    }
+
+    Insert(tab, fields){
+        return connectionPool.then(pool =>{
+            const req = pool.request();
+            let command = `${dbname} INSERT INTO ${tab} values (`;
+            Object.keys(fields).forEach(field =>{
+                let fieldType = Number.isInteger(fields[field]) ? sql.Int : sql.NVarChar;
+                req.input(field, fieldType, fields[field]);
+                command += `@${field},`;
+            });
+            command = command.replace(/.$/,")");
+
+            return req.query(command);
+        });
+    }
+
+    Delete(tab, id){
+        return connectionPool.then(pool =>{
+          console.log(`${dbname} DELETE FROM ${tab} WHERE ${tab} = ${id}`);
+          if (!id) throw 'Problem with ID';
+
+          return pool.query(`${dbname} DELETE FROM ${tab} WHERE ${tab} = '${id}'`);
+        });
+    }
+}
+
+module.exports = DB;
